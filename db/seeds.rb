5bb35f68d54316cfb36e1d471145f0a080fb6466
ediboss <edigenuis@gmail.com>
2017-08-10 00:39:32 +0000

    init

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(username: "esaidy",
+User.create!(username: "esaidy1",
 			full_name: "Ebrima Saidy",
 			department: "Sales",
 			password: "$2a$10$DcJOlz.peqUOQqd2L6yJt.BocSUsZRuDEgSXtbjk7cgqq3xF0RfOO",
